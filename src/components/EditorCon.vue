5465759bad17bb5d0265499b45aa30d544abae43
asuishi <1053562072@qq.com>
2023-04-27 17:40:33 +0800

    update

---
+++ src/components/EditorCon.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+// import FileSelector from './FileSelector.vue'
+import CodeMirror from './codemirror/CodeMirror.vue'
+// import Message from '../Message.vue'
+import { debounce } from '../utils'
+import { ref } from 'vue'
+const value = ref<string>('')
+
+const onChange = debounce((code: string) => {
+  console.log(code)
+}, 250)
+
+// const activeMode = computed(() => {
+//   const { filename } = store.state.activeFile
+//   return filename.endsWith('.vue') || filename.endsWith('.html')
+//     ? 'htmlmixed'
+//     : filename.endsWith('.css')
+//     ? 'css'
+//     : 'javascript'
+// })
+</script>
+
+<template>
+  <div class="editor-container">
+    <CodeMirror
+      @change="onChange"
+      :value="value"
+      :mode="activeMode"
+    />
+  </div>
+</template>
+
+<style scoped>
+.editor-container {
+  background-color: aqua;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+</style>
---
+++ src/components/output/Output.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import CodeMirror from '../codemirror/CodeMirror.vue'
+import {  ref } from 'vue'
+const value = ref<string>('const a = 5')
+
+</script>
+
+<template>
+  <div class="output-container">
+    <CodeMirror
+      v-if="mode !== 'preview'"
+      readonly
+      :mode="mode === 'css' ? 'css' : 'javascript'"
+      :value="value"
+    />
+  </div>
+</template>
+
+<style scoped>
+.output-container {
+  background-color: var(--bg-soft);
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+}
+
+.tab-buttons {
+  box-sizing: border-box;
+  border-bottom: 1px solid var(--border);
+  background-color: var(--bg);
+  height: var(--header-height);
+  overflow: hidden;
+}
+.tab-buttons button {
+  padding: 0;
+  box-sizing: border-box;
+}
+.tab-buttons span {
+  font-size: 13px;
+  font-family: var(--font-code);
+  text-transform: uppercase;
+  color: var(--text-light);
+  display: inline-block;
+  padding: 8px 16px 6px;
+  line-height: 20px;
+}
+button.active {
+  color: var(--color-branding-dark);
+  border-bottom: 3px solid var(--color-branding-dark);
+}
+</style>
