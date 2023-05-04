1bd1d7726bbb372aba34d125c374b5852e4b53d9
asuishi <1053562072@qq.com>
2023-05-04 17:01:49 +0800

    fix

--- env.d.ts
+++ env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 declare module 'compiler-step' {
-  export function parseTemplate(args: any[]): any;
-  export function transformTemplate(args: any[]): any;
-  export function generateTemplate(args: any[]): any;
+  export function parseTemplate(...args: any[]): any;
+  export function transformTemplate(...args: any[]): any;
+  export function generateTemplate(...args: any[]): any;
 }
